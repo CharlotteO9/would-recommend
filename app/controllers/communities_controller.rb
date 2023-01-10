a0fd4d4c335e58697afe9a6ed7a2afa316db9f36
charlotte oswald <charlotteoswald9@gmail.com>
2023-01-10 17:33:39 +0100

    communities controller is done, need to check with view if everything is working

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -12,10 +12,23 @@ class CommunitiesController < ApplicationController
   end
 
   def create
-
+    @community = Community.new(params_communities)
+    if @community.save
+      redirect_to community_path(@community)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @community = Community.find(params[:id])
+    @community.destroy
+    redirect_to communities_path, notice: "Community deleted!"
+  end
 
+  private
+
+  def params_communities
+    params.require(:restaurant).permit(:name, :address, :rating)
   end
 end
